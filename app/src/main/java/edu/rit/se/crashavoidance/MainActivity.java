0786c2869081c3edcb0604b0570f280eb5453724
Brett Tuttle <tuttle.brett@gmail.com>
2016-02-02 19:37:38 -0500

    Started modifying based on the services demo

--- app/src/main/java/edu/rit/se/crashavoidance/MainActivity.java
+++ app/src/main/java/edu/rit/se/crashavoidance/MainActivity.java
@@ -1,29 +1,51 @@
 package edu.rit.se.crashavoidance;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends Activity implements ServicesList.DeviceClickListener {
+
+    public static final String SERVICE_NAME = "_crashavoidance";
+    static final int SERVER_PORT = 4545;
+    public static final int MESSAGE_READ = 0x400 + 1;
+    public static final int MY_HANDLE = 0x400 + 2;
 
     private IntentFilter intentFilter = new IntentFilter();
     private WifiP2pManager mManager;
     private WifiP2pManager.Channel mChannel;
+    private TextView statusTextView;
+    private WifiP2pDnsSdServiceRequest serviceRequest;
+    private BroadcastReceiver receiver = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -42,7 +64,22 @@ public class MainActivity extends AppCompatActivity {
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
 
+        statusTextView = (TextView) findViewById(R.id.status_text);
 
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 
     @Override
@@ -65,5 +102,179 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void registerAndFindServices() {
+        Map<String, String> record = new HashMap<String, String>();
+        record.put("available", "visible");
+
+        WifiP2pDnsSdServiceInfo service = WifiP2pDnsSdServiceInfo.newInstance(
+                SERVICE_NAME, "_presence._tcp", record);
+        mManager.addLocalService(mChannel, service, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                appendStatus("Added Local Service");
+            }
+
+            @Override
+            public void onFailure(int error) {
+                appendStatus("Failed to add a service");
+            }
+        });
+
+        discoverService();
+    }
+
+    private void discoverService() {
+        /*
+         * Register listeners for DNS-SD services. These are callbacks invoked
+         * by the system when a service is actually discovered.
+         */
+
+        mManager.setDnsSdResponseListeners(mChannel,
+                new WifiP2pManager.DnsSdServiceResponseListener() {
+
+                    @Override
+                    public void onDnsSdServiceAvailable(String instanceName,
+                                                        String registrationType, WifiP2pDevice srcDevice) {
+
+                        // A service has been discovered. Is this our app?
+
+                        if (instanceName.equalsIgnoreCase(SERVICE_NAME)) {
+
+                            // update the UI and add the item the discovered
+                            // device.
+                            ServicesList fragment = (ServicesList) getFragmentManager()
+                                    .findFragmentByTag("services");
+                            if (fragment != null) {
+                                ServicesList.WiFiDevicesAdapter adapter = ((ServicesList.WiFiDevicesAdapter) fragment
+                                        .getListAdapter());
+                                WiFiP2pService service = new WiFiP2pService();
+                                service.device = srcDevice;
+                                service.instanceName = instanceName;
+                                service.serviceRegistrationType = registrationType;
+                                adapter.add(service);
+                                adapter.notifyDataSetChanged();
+                                Log.d(SERVICE_NAME, "onBonjourServiceAvailable "
+                                        + instanceName);
+                            }
+                        }
+
+                    }
+                }, new WifiP2pManager.DnsSdTxtRecordListener() {
+
+                    /**
+                     * A new TXT record is available. Pick up the advertised
+                     * buddy name.
+                     */
+                    @Override
+                    public void onDnsSdTxtRecordAvailable(
+                            String fullDomainName, Map<String, String> record,
+                            WifiP2pDevice device) {
+                        Log.d(SERVICE_NAME,
+                                device.deviceName + " is "
+                                        + record.get("available"));
+                    }
+                });
+
+        // After attaching listeners, create a service request and initiate
+        // discovery.
+        serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
+        mManager.addServiceRequest(mChannel, serviceRequest,
+                new WifiP2pManager.ActionListener() {
+
+                    @Override
+                    public void onSuccess() {
+                        appendStatus("Added service discovery request");
+                    }
+
+                    @Override
+                    public void onFailure(int arg0) {
+                        appendStatus("Failed adding service discovery request");
+                    }
+                });
+        mManager.discoverServices(mChannel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                appendStatus("Service discovery initiated");
+            }
+
+            @Override
+            public void onFailure(int arg0) {
+                appendStatus("Service discovery failed");
+
+            }
+        });
+    }
+
+    @Override
+    public void connectP2p(WiFiP2pService service) {
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = service.device.deviceAddress;
+        config.wps.setup = WpsInfo.PBC;
+        if (serviceRequest != null)
+            mManager.removeServiceRequest(mChannel, serviceRequest,
+                    new WifiP2pManager.ActionListener() {
+
+                        @Override
+                        public void onSuccess() {
+                        }
+
+                        @Override
+                        public void onFailure(int arg0) {
+                        }
+                    });
+
+        mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                appendStatus("Connecting to service");
+            }
+
+            @Override
+            public void onFailure(int errorCode) {
+                appendStatus("Failed connecting to service");
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionInfoAvailable(WifiP2pInfo p2pInfo) {
+        Thread handler = null;
+        /*
+         * The group owner accepts connections using a server socket and then spawns a
+         * client socket for every client. This is handled by {@code
+         * GroupOwnerSocketHandler}
+         */
+
+        if (p2pInfo.isGroupOwner) {
+            Log.d(SERVICE_NAME, "Connected as group owner");
+            try {
+                handler = new GroupOwnerSocketHandler(
+                        ((MessageTarget) this).getHandler());
+                handler.start();
+            } catch (IOException e) {
+                Log.d(SERVICE_NAME,
+                        "Failed to create a server thread - " + e.getMessage());
+                return;
+            }
+        } else {
+            Log.d(SERVICE_NAME, "Connected as peer");
+            handler = new ClientSocketHandler(
+                    ((MessageTarget) this).getHandler(),
+                    p2pInfo.groupOwnerAddress);
+            handler.start();
+        }
+        chatFragment = new WiFiChatFragment();
+        getFragmentManager().beginTransaction()
+                .replace(R.id.main_container, chatFragment).commit();
+        statusTxtView.setVisibility(View.GONE);
+    }
+
+    private void appendStatus(String status) {
+        statusTextView.append("\n" + status);
     }
 }
---
+++ app/src/main/java/edu/rit/se/crashavoidance/WiFiP2pService.java
@@ -0,0 +1,12 @@
+package edu.rit.se.crashavoidance;
+
+import android.net.wifi.p2p.WifiP2pDevice;
+
+/**
+ * Created by Brett on 2/2/2016.
+ */
+public class WiFiP2pService {
+    WifiP2pDevice device;
+    String instanceName = null;
+    String serviceRegistrationType = null;
+}
