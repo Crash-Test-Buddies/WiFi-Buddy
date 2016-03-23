0d025c7c671d89ef2425a71f76d017afd461ef9a
Brendan Kirby <bjk7706@gmail.com>
2016-03-23 14:54:04 -0400

    Comments old unused code for now

--- app/src/main/java/edu/rit/se/crashavoidance/ClientSocketHandler.java
+++ app/src/main/java/edu/rit/se/crashavoidance/ClientSocketHandler.java
@@ -1,48 +1,48 @@
-package edu.rit.se.crashavoidance;
-
-import android.os.Handler;
-import android.util.Log;
-
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-
-public class ClientSocketHandler extends Thread {
-
-    private static final String TAG = "ClientSocketHandler";
-    private Handler handler;
-    private ChatManager chat;
-    private InetAddress mAddress;
-
-    public ClientSocketHandler(Handler handler, InetAddress groupOwnerAddress) {
-        this.handler = handler;
-        this.mAddress = groupOwnerAddress;
-    }
-
-    @Override
-    public void run() {
-        Socket socket = new Socket();
-        try {
-            socket.bind(null);
-            socket.connect(new InetSocketAddress(mAddress.getHostAddress(),
-                    MainActivity.SERVER_PORT), 5000);
-            Log.d(TAG, "Launching the I/O handler");
-            chat = new ChatManager(socket, handler);
-            new Thread(chat).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-            try {
-                socket.close();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            return;
-        }
-    }
-
-    public ChatManager getChat() {
-        return chat;
-    }
-
-}
+//package edu.rit.se.crashavoidance;
+//
+//import android.os.Handler;
+//import android.util.Log;
+//
+//import java.io.IOException;
+//import java.net.InetAddress;
+//import java.net.InetSocketAddress;
+//import java.net.Socket;
+//
+//public class ClientSocketHandler extends Thread {
+//
+//    private static final String TAG = "ClientSocketHandler";
+//    private Handler handler;
+//    private ChatManager chat;
+//    private InetAddress mAddress;
+//
+//    public ClientSocketHandler(Handler handler, InetAddress groupOwnerAddress) {
+//        this.handler = handler;
+//        this.mAddress = groupOwnerAddress;
+//    }
+//
+//    @Override
+//    public void run() {
+//        Socket socket = new Socket();
+//        try {
+//            socket.bind(null);
+//            socket.connect(new InetSocketAddress(mAddress.getHostAddress(),
+//                    MainActivity.SERVER_PORT), 5000);
+//            Log.d(TAG, "Launching the I/O handler");
+//            chat = new ChatManager(socket, handler);
+//            new Thread(chat).start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            try {
+//                socket.close();
+//            } catch (IOException e1) {
+//                e1.printStackTrace();
+//            }
+//            return;
+//        }
+//    }
+//
+//    public ChatManager getChat() {
+//        return chat;
+//    }
+//
+//}
