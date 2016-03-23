68e7e7e1ba771f2d3fcecf811fa42db95e1dae99
Brendan Kirby <bjk7706@gmail.com>
2016-03-22 20:33:37 -0400

    Adds menu to initActivity

--- app/src/main/java/edu/rit/se/crashavoidance/initActivity.java
+++ app/src/main/java/edu/rit/se/crashavoidance/initActivity.java
@@ -1,16 +1,19 @@
 package edu.rit.se.crashavoidance;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import edu.rit.se.crashavoidance.views.AvailableServicesActivity;
 
-public class initActivity extends Activity {
+public class initActivity extends AppCompatActivity {
 
     WifiManager wifiManager;
 
@@ -19,7 +22,8 @@ public class initActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
 
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.initToolbar);
+        setSupportActionBar(toolbar);
 
         findViewById(R.id.wifiToggle_btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +50,48 @@ public class initActivity extends Activity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.init_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_disconnect:
+                onClickMenuDisconnect(item);
+                return true;
+            case R.id.action_toggle_wifi:
+                onClickMenuToggleWifi(item);
+                return true;
+            case R.id.action_view_logs:
+                onClickMenuViewLogs(item);
+                return true;
+            case R.id.action_exit:
+                onClickMenuExit(item);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onClickMenuDisconnect(MenuItem item) {
+        displayToast("Disconnect tapped");
+    }
+
+    public void onClickMenuToggleWifi(MenuItem item) {
+        displayToast("Toggle Wi-Fi tapped");
+    }
+
+    public void onClickMenuViewLogs(MenuItem item) {
+        displayToast("View Logs tapped");
+    }
+
+    public void onClickMenuExit(MenuItem item) {
+        finish();
+    }
 
     private void toggleWifi(){
         if(wifiManager.isWifiEnabled()){
@@ -62,5 +108,10 @@ public class initActivity extends Activity {
     private void scanForServices(){
         Intent intent = new Intent(this, AvailableServicesActivity.class);
         startActivity(intent);
+    }
+
+    public void displayToast(String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
