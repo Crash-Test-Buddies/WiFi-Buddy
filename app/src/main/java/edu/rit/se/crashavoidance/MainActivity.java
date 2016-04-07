8985a8193d86e018e080b18625bc7e6726e860fa
Brendan Kirby <brendan.kirby@rit.edu>
2016-04-07 15:57:35 -0400

    Makes MainActivity the starting activity. Using MainFragment now

--- app/src/main/java/edu/rit/se/crashavoidance/MainActivity.java
+++ app/src/main/java/edu/rit/se/crashavoidance/MainActivity.java
@@ -71,7 +71,7 @@
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main_old);
 //
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
