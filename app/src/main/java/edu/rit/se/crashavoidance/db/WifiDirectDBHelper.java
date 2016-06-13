1a4303119ee6c05909c2cde43c5e7015fb29deb3
Chris <celdridge91190@gmail.com>
2016-06-13 05:57:51 -0400

    Added drop tables call to onUpgrade

--- app/src/main/java/edu/rit/se/crashavoidance/db/WifiDirectDBHelper.java
+++ app/src/main/java/edu/rit/se/crashavoidance/db/WifiDirectDBHelper.java
@@ -75,7 +75,7 @@ public class WifiDirectDBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
-//        db.execSQL(SQL_DELETE_ENTRIES);
+        dropTables(db);
         onCreate(db);
     }
 
